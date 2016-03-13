ded60dfe33e2b72232e02a8e70b930251511f186
Alexandre Mutel <alexandre_mutel@live.com>
2016-03-13 10:19:57 +0900

    Migrate Scriban project to xproj + project.json

--- src/Textamina.Scriban/Properties/AssemblyInfo.cs
+++ src/Textamina.Scriban/Properties/AssemblyInfo.cs
@@ -1,20 +1,17 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Textamin")]
+[assembly: AssemblyTitle("Textamina.Scriban")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Textamin")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Textamina.Scriban")]
+[assembly: AssemblyCopyright("Copyright © 2016 Alexandre Mutel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
