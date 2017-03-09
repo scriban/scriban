7cee9161ba337b727f4291069d9eb11cc6c2c36f
Alexandre Mutel <alexandre_mutel@live.com>
2017-03-09 18:41:06 +0100

    Bump version to 0.3.0

--- src/Scriban/Properties/AssemblyInfo.cs
+++ src/Scriban/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.2")]
-[assembly: AssemblyFileVersion("0.2.2")]
+[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyFileVersion("0.3.0")]
 
 [assembly: InternalsVisibleTo("Scriban.Tests")]
 
