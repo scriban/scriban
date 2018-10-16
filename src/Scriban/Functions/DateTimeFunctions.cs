f5743b2767a1ef404280d164f74322c207feed3d
Matthias Reitinger <reima@edenprime.de>
2018-10-17 00:53:16 +0200

    Add missing datetime formatting codes (#81)

--- src/Scriban/Functions/DateTimeFunctions.cs
+++ src/Scriban/Functions/DateTimeFunctions.cs
@@ -53,16 +53,34 @@ namespace Scriban.Functions
             { 'b', (dateTime, cultureInfo) => dateTime.ToString("MMM", cultureInfo) },
             { 'B', (dateTime, cultureInfo) => dateTime.ToString("MMMM", cultureInfo) },
             { 'c', (dateTime, cultureInfo) => dateTime.ToString("ddd MMM dd HH:mm:ss yyyy", cultureInfo) },
+            { 'C', (dateTime, cultureInfo) => (dateTime.Year / 100).ToString("D2", cultureInfo) },
             { 'd', (dateTime, cultureInfo) => dateTime.ToString("dd", cultureInfo) },
+            { 'D', (dateTime, cultureInfo) => dateTime.ToString("MM/dd/yy", cultureInfo) },
             { 'e', (dateTime, cultureInfo) => dateTime.ToString("%d", cultureInfo).PadLeft(2, ' ') },
+            { 'F', (dateTime, cultureInfo) => dateTime.ToString("yyyy-MM-dd", cultureInfo) },
+            { 'h', (dateTime, cultureInfo) => dateTime.ToString("MMM", cultureInfo) },
             { 'H', (dateTime, cultureInfo) => dateTime.ToString("HH", cultureInfo) },
             { 'I', (dateTime, cultureInfo) => dateTime.ToString("hh", cultureInfo) },
             { 'j', (dateTime, cultureInfo) => dateTime.DayOfYear.ToString("D3", cultureInfo) },
+            { 'k', (dateTime, cultureInfo) => dateTime.ToString("%H", cultureInfo).PadLeft(2, ' ') },
+            { 'l', (dateTime, cultureInfo) => dateTime.ToString("%h", cultureInfo).PadLeft(2, ' ') },
+            { 'L', (dateTime, cultureInfo) => dateTime.ToString("FFF", cultureInfo) },
             { 'm', (dateTime, cultureInfo) => dateTime.ToString("MM", cultureInfo) },
-            { 'M', (dateTime, cultureInfo) => dateTime.Minute.ToString("D2", cultureInfo) },
+            { 'M', (dateTime, cultureInfo) => dateTime.ToString("mm", cultureInfo) },
+            { 'n', (dateTime, cultureInfo) => "\n" },
+            { 'N', (dateTime, cultureInfo) => dateTime.ToString("fffffff00", cultureInfo) },
             { 'p', (dateTime, cultureInfo) => dateTime.ToString("tt", cultureInfo) },
+            { 'P', (dateTime, cultureInfo) => dateTime.ToString("tt", cultureInfo).ToLowerInvariant() },
+            { 'r', (dateTime, cultureInfo) => dateTime.ToString("hh:mm:ss tt", cultureInfo) },
+            { 'R', (dateTime, cultureInfo) => dateTime.ToString("HH:mm", cultureInfo) },
+            { 's', (dateTime, cultureInfo) => ((dateTime.ToUniversalTime().Ticks - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks) / TimeSpan.TicksPerSecond).ToString(cultureInfo) },
             { 'S', (dateTime, cultureInfo) => dateTime.ToString("ss", cultureInfo) },
+            { 't', (dateTime, cultureInfo) => "\t" },
+            { 'T', (dateTime, cultureInfo) => dateTime.ToString("HH:mm:ss", cultureInfo) },
+            { 'u', (dateTime, cultureInfo) => ((dateTime.DayOfWeek == DayOfWeek.Sunday) ? 7 : (int) dateTime.DayOfWeek).ToString(cultureInfo) },
             { 'U', (dateTime, cultureInfo) => cultureInfo.Calendar.GetWeekOfYear(dateTime, cultureInfo.DateTimeFormat.CalendarWeekRule, DayOfWeek.Sunday).ToString("D2", cultureInfo) },
+            { 'v', (dateTime, cultureInfo) => string.Format(CultureInfo.InvariantCulture, "{0,2}-{1}-{2:D4}", dateTime.Day, dateTime.ToString("MMM", CultureInfo.InvariantCulture).ToUpper(), dateTime.Year) },
+            { 'V', (dateTime, cultureInfo) => cultureInfo.Calendar.GetWeekOfYear(dateTime, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday).ToString("D2", cultureInfo) },
             { 'W', (dateTime, cultureInfo) => cultureInfo.Calendar.GetWeekOfYear(dateTime, cultureInfo.DateTimeFormat.CalendarWeekRule, DayOfWeek.Monday).ToString("D2", cultureInfo) },
             { 'w', (dateTime, cultureInfo) => ((int) dateTime.DayOfWeek).ToString(cultureInfo) },
             { 'x', (dateTime, cultureInfo) => dateTime.ToString("d", cultureInfo) },
@@ -244,45 +262,63 @@ namespace Scriban.Functions
         /// <summary>
         /// Converts a datetime object to a textual representation using the specified format string.
         /// 
-        /// By default, if you are using a date, it will use the format specified by `date.format` which defaults to /// `date.default_format` (readonly) which default to `%d %b %Y`
+        /// By default, if you are using a date, it will use the format specified by `date.format` which defaults to `date.default_format` (readonly) which default to `%d %b %Y`
         /// 
-        /// You can override the format used for formatting all dates by assigning the a new format: `date.format = '%a /// %b %e %T %Y';`
+        /// You can override the format used for formatting all dates by assigning the a new format: `date.format = '%a %b %e %T %Y';`
         /// 
         /// You can recover the default format by using `date.format = date.default_format;`
         /// 
-        /// By default, the to_string format is using the **current culture**, but you can switch to an invariant /// culture by using the modifier `%g`
+        /// By default, the to_string format is using the **current culture**, but you can switch to an invariant culture by using the modifier `%g`
         /// 
         /// For example, using `%g %d %b %Y` will output the date using an invariant culture.
         /// 
         /// If you are using `%g` alone, it will output the date with `date.format` using an invariant culture.
         /// 
-        /// Suppose that `date.now` would return the date `2013-09-12 22:49:27 +0530`, the following table explains the /// format modifiers:
+        /// Suppose that `date.now` would return the date `2013-09-12 22:49:27 +0530`, the following table explains the format modifiers:
         /// 
-        /// | Format | Result        | Description
-        /// |--------|---------------|--------------------------------------------
-        /// | `"%a"` |  `"Thu"` 	   | Name of week day in short form of the
-        /// | `"%A"` |  `"Thursday"` | Week day in full form of the time
-        /// | `"%b"` |  `"Sep"` 	   | Month in short form of the time
-        /// | `"%B"` |  `"September"`| Month in full form of the time
-        /// | `"%c"` |               | Date and time (%a %b %e %T %Y)
-        /// | `"%d"` |  `"12"` 	     | Day of the month of the time
-        /// | `"%e"` |  `"12"`       | Day of the month, blank-padded ( 1..31)
-        /// | `"%H"` |  `"22"`       | Hour of the time in 24 hour clock format
-        /// | `"%I"` |  `"10"` 	     | Hour of the time in 12 hour clock format
-        /// | `"%j"` |               | Day of the year (001..366) (3 digits, left padded with zero)
-        /// | `"%m"` |  `"09"` 	     | Month of the time
-        /// | `"%M"` |  `"49"` 	     | Minutes of the time (2 digits, left padded with zero e.g 01 02)
-        /// | `"%p"` |  `"PM"` 	     | Gives AM / PM of the time
-        /// | `"%S"` |  `"27"` 	     | Seconds of the time
-        /// | `"%U"` |               | Week number of the current year, starting with the first Sunday as the first day /// of the first week (00..53)
-        /// | `"%W"` |               | Week number of the current year, starting with the first Monday as the first day /// of the first week (00..53)
-        /// | `"%w"` |  `"4"` 	     | Day of week of the time
-        /// | `"%x"` |               | Preferred representation for the date alone, no time
-        /// | `"%X"` |               | Preferred representation for the time alone, no date
-        /// | `"%y"` |  `"13"` 	     | Gives year without century of the time
-        /// | `"%Y"` |  `"2013"`     | Year of the time
-        /// | `"%Z"` |  `"IST"` 	   | Gives Time Zone of the time
-        /// | `"%%"` |  `"%"`        | Output the character `%`
+        /// | Format | Result            | Description
+        /// |--------|-------------------|--------------------------------------------
+        /// | `"%a"` |  `"Thu"`          | Name of week day in short form of the
+        /// | `"%A"` |  `"Thursday"`     | Week day in full form of the time
+        /// | `"%b"` |  `"Sep"`          | Month in short form of the time
+        /// | `"%B"` |  `"September"`    | Month in full form of the time
+        /// | `"%c"` |                   | Date and time (%a %b %e %T %Y)
+        /// | `"%C"` |  `"20"`           | Century of the time
+        /// | `"%d"` |  `"12"`           | Day of the month of the time
+        /// | `"%D"` |  `"09/12/13"`     | Date (%m/%d/%y)
+        /// | `"%e"` |  `"12"`           | Day of the month, blank-padded ( 1..31)
+        /// | `"%F"` |  `"2013-09-12"`   | ISO 8601 date (%Y-%m-%d)
+        /// | `"%h"` |  `"Sep"`          | Alias for %b
+        /// | `"%H"` |  `"22"`           | Hour of the time in 24 hour clock format
+        /// | `"%I"` |  `"10"`           | Hour of the time in 12 hour clock format
+        /// | `"%j"` |  `"255"`          | Day of the year (001..366) (3 digits, left padded with zero)
+        /// | `"%k"` |  `"22"`           | Hour of the time in 24 hour clock format, blank-padded ( 0..23)
+        /// | `"%l"` |  `"10"`           | Hour of the time in 12 hour clock format, blank-padded ( 0..12)
+        /// | `"%L"` |  `"000"`          | Millisecond of the time (3 digits, left padded with zero)
+        /// | `"%m"` |  `"09"`           | Month of the time
+        /// | `"%M"` |  `"49"`           | Minutes of the time (2 digits, left padded with zero e.g 01 02)
+        /// | `"%n"` |                   | Newline character (\n)
+        /// | `"%N"` |  `"000000000"`    | Nanoseconds of the time (9 digits, left padded with zero)
+        /// | `"%p"` |  `"PM"`           | Gives AM / PM of the time
+        /// | `"%P"` |  `"pm"`           | Gives am / pm of the time
+        /// | `"%r"` |  `"10:49:27 PM"`  | Long time in 12 hour clock format (%I:%M:%S %p)
+        /// | `"%R"` |  `"22:49"`        | Short time in 24 hour clock format (%H:%M)
+        /// | `"%s"` |                   | Number of seconds since 1970-01-01 00:00:00 +0000
+        /// | `"%S"` |  `"27"`           | Seconds of the time
+        /// | `"%t"` |                   | Tab character (\t)
+        /// | `"%T"` |  `"22:49:27"`     | Long time in 24 hour clock format (%H:%M:%S)
+        /// | `"%u"` |  `"4"`            | Day of week of the time (from 1 for Monday to 7 for Sunday)
+        /// | `"%U"` |  `"36"`           | Week number of the current year, starting with the first Sunday as the first day of the first week (00..53)
+        /// | `"%v"` |  `"12-SEP-2013"`  | VMS date (%e-%b-%Y) (culture invariant)
+        /// | `"%V"` |  `"37"`           | Week number of the current year according to ISO 8601 (01..53)
+        /// | `"%W"` |  `"36"`           | Week number of the current year, starting with the first Monday as the first day of the first week (00..53)
+        /// | `"%w"` |  `"4"`            | Day of week of the time (from 0 for Sunday to 6 for Saturday)
+        /// | `"%x"` |                   | Preferred representation for the date alone, no time
+        /// | `"%X"` |                   | Preferred representation for the time alone, no date
+        /// | `"%y"` |  `"13"`           | Gives year without century of the time
+        /// | `"%Y"` |  `"2013"`         | Year of the time
+        /// | `"%Z"` |  `"IST"`          | Gives Time Zone of the time
+        /// | `"%%"` |  `"%"`            | Output the character `%`
         /// 
         /// Note that the format is using a good part of the ruby format ([source]/// (http://apidock.com/ruby/DateTime/strftime))
         /// ```scriban-html
