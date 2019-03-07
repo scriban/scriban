7c0e22ea72cafecadc784270e65c57ce8c360457
Alexandre Mutel <alexandre@unity3d.com>
2019-03-07 21:32:23 +0100

    Add missing file

---
+++ src/Scriban/Helpers/BoxHelper.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Alexandre Mutel. All rights reserved.
+// Licensed under the BSD-Clause 2 license. 
+namespace Scriban.Helpers
+{
+    /// <summary>
+    /// Provides true/false already boxed
+    /// </summary>
+    internal static class BoxHelper
+    {
+        public static readonly object TrueObject = true;
+        public static readonly object FalseObject = false;
+    }
+}
