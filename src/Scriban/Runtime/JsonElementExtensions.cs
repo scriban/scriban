170b2de7363f33c6d2e20b01615d0d1171b103bf
kcalderon37 <38506224+kcalderon37@users.noreply.github.com>
2025-03-04 17:28:16 -0600

    Update JsonElementExtensions.cs Add #nullable enable
    This is just to fix issue detailed here:
    
    https://github.com/scriban/scriban/issues/591

--- src/Scriban/Runtime/JsonElementExtensions.cs
+++ src/Scriban/Runtime/JsonElementExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the BSD-Clause 2 license.
 // See license.txt file in the project root for full license information.
 
+#nullable enable
 #if NET
 using System;
 using System.Text.Json;
